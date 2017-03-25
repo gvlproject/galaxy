c7edea42e1365af8cabbacb6aed74c57eaa87e6c
Nuwan Goonasekera <nuwan.ag@gmail.com>
2017-03-25 18:09:31 +0530

    Merged patch for GenomeSpace workflow support

--- client/galaxy/scripts/mvc/form/form-parameters.js
+++ client/galaxy/scripts/mvc/form/form-parameters.js
@@ -6,8 +6,9 @@ define(['utils/utils',
         'mvc/ui/ui-select-content',
         'mvc/ui/ui-select-library',
         'mvc/ui/ui-select-ftp',
+        'mvc/ui/ui-select-genomespace',
         'mvc/ui/ui-color-picker'],
-    function( Utils, Ui, SelectContent, SelectLibrary, SelectFtp, ColorPicker ) {
+    function( Utils, Ui, SelectContent, SelectLibrary, SelectFtp, SelectGenomeSpace, ColorPicker ) {
 
     // create form view
     return Backbone.Model.extend({
@@ -29,7 +30,8 @@ define(['utils/utils',
             'hidden_data'       : '_fieldHidden',
             'baseurl'           : '_fieldHidden',
             'library_data'      : '_fieldLibrary',
-            'ftpfile'           : '_fieldFtp'
+            'ftpfile'           : '_fieldFtp',
+            'genomespacefile'   : '_fieldGenomeSpace'
         },
 
         /** Returns an input field for a given field type */
@@ -210,6 +212,17 @@ define(['utils/utils',
                 optional    : input_def.optional,
                 multiple    : input_def.multiple,
                 onchange    : input_def.onchange
+            });
+            },
+
+        /** GenomeSpace file select field */
+        _fieldGenomeSpace: function( input_def ) {
+            var self = this;
+            return new SelectGenomeSpace.View({
+                id          : 'field-' + input_def.id,
+                onchange    : function() {
+                    self.app.trigger( 'change' );
+                }
             });
         }
     });
--- lib/galaxy/web/form_builder.py
+++ lib/galaxy/web/form_builder.py
@@ -206,6 +206,25 @@ class FileField(BaseField):
         return unicodify( '<input type="file" name="%s%s"%s%s>' % ( prefix, self.name, ajax_text, value_text ) )
 
 
+class GenomespaceFileField(BaseField):
+    """
+    A genomspace file browser field.
+    >>> print GenomespaceFileField( "foo", 100 ).get_html()
+    <input type="text" name="foo" ...>
+    """
+    def __init__(self, name, value=None):
+        self.name = name
+        self.value = value or ""
+
+    def get_html(self, prefix=""):
+        return unicodify('<script src="https://gsui.genomespace.org/jsui/upload/gsuploadwindow.js"></script>'
+                         '<input type="text" name="{0}{1}" value="{2}">&nbsp;'
+                         '<a href="javascript:gsLocationByGet({{ successCallback: function(config)'
+                         ' {{ selector_name = \'{0}{1}\'; selector = \'input[name=\' + selector_name.replace(\'|\', \'\\\\|\') + \']\';'
+                         ' $(selector).val(config.destination + \'^\' + config.token); }} }});">'
+                         'Browse</a>'.format(prefix, self.name, escape(str(self.value), quote=True)))
+
+
 class HiddenField(BaseField):
     """
     A hidden field.
